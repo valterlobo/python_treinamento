9c19b70aa3d88db37d489f53e7590eefe598b65d
valterlobo <valterlobo@gmail.com>
2017-03-21 01:16:23 -0300

    -config  conceito

---
+++ config/config.py
@@ -0,0 +1,10 @@
+
+theme = dict(
+    color='blue',
+    brand='ford',
+)
+city = 'new york'
+database = dict(
+    user='black',
+    passw='white',
+    )
---
+++ config/configteste.py
@@ -0,0 +1,10 @@
+
+
+import config
+
+
+print config.theme['color']
+print config.database['user']
+print config.database['passw']
+
+print config.city
